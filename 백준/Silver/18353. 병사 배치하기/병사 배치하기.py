16695133bcdb21ccdf3af04ced3dfe4d009c5b4b
inns21 <127088920+inns21@users.noreply.github.com>
2025-01-18 15:21:32 +0900

    [Silver II] Title: 병사 배치하기, Time: 100 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -0,0 +1,20 @@
+n = int(input())
+n_list = list(map(int, input().split()))
+
+r_list = list()
+r_list.append(n_list[0])
+for i in range(1,n):
+  if r_list[-1] > n_list[i]:
+    r_list.append(n_list[i])
+  else:
+    start = 0
+    end = len(r_list)-1
+    while start <= end:
+      mid = (start+end) // 2
+      if r_list[mid] > n_list[i]:
+        start = mid + 1
+      else:
+        end = mid - 1
+    r_list[start] = n_list[i]
+
+print(n-len(r_list))
